b628e1bb443d24735b037fb780f277560358e9a4
Robin Sheong <sheong2@illinois.edu>
2019-03-25 20:10:20 -0500

    multiple nodes appear, added links to list

--- d3/scripts.js
+++ d3/scripts.js
@@ -2,7 +2,8 @@
 let w = window.innerWidth,
     h = window.innerHeight;
 
-let svg = d3.select('body').append('svg')
+let svg = d3.select('body')
+    .append('svg')
     .attr('width', w)
     .attr('height', h);
 
@@ -14,26 +15,49 @@ d3.json(data_mini, function(error, json) {
     if (error) console.log(error);
     let rawData = json;
     let data = [];
+    let connections = [];
     var numTopics = Object.keys(rawData).length; // length of outermost JSON object
     console.log(numTopics);
 
     //For each topic rawData[i]
     for(var i = 0; i<numTopics; i++){
-        data.push({'tweets': rawData[i].tweets, 'words': rawData[i].words});
+        data.push({'id': i, 'tweets': rawData[i].tweets, 'words': rawData[i].words});
+        // data is a list of objects which contain lists?
     }
 
-    dataReady(data);
+    data.forEach(function(d){
+        for (var i = 0; i<numTopics; i++){
+            for (var j = i+1; j<numTopics; j++){
+                if(i != d.id) { //this might be wrong
+                    connections.push({source: d.id, target: i});
+                }
+            }
+        }
+    });
+    console.log(connections);
+    dataReady(data, connections);
 });
 
-function dataReady(data){
+function dataReady(data, connections){
+    let radius = 100;
+
     let force = d3.layout.force()
         .nodes(data)
-        .charge(-100)
+        .linkDistance(radius)
         .on('tick', tick)
         .start();
 
     let nodes = svg.selectAll('.node')
         .data(data);
+
+    let links = svg.selectAll('.line')
+        .data(connections);
+
+    console.log(links);
+
+    let linkEnter = links.enter().append('line');
+
+    console.log(nodes);
 
     let nodeEnter = nodes.enter().append('g')
         .attr('class', 'node');
@@ -41,24 +65,30 @@ function dataReady(data){
     nodeEnter.append('circle')
         .attr('cx', 0)
         .attr('cy', 0)
-        .attr('r', 30);
+        .attr('r', radius);
 
     nodeEnter.append('text')
         .attr('text-anchor', 'middle')
         .attr('dy', '0.34em');
 
+
     svg.selectAll("circle").on("click", function(){
-        d3.select(this).attr('r', 25)
+        d3.select(this).attr('r', radius)
             .style("fill","lightcoral")
             .style("stroke","red");
     });
 
     function tick() {
-        nodes.attr('transform', function(d) {
-            return 'translate(' + d.x + ',' + d.y + ')';
-        });
+        svg.selectAll("circle").attr("cx", function(d) { return d.x = Math.max(radius, Math.min(w - radius, d.x)); })
+            .attr("cy", function(d) { return d.y = Math.max(radius, Math.min(h - radius, d.y)); });
+
         nodes.select('text').text(function(d) {
             return "test text";
         });
+
+        linkEnter.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
     }
 }
